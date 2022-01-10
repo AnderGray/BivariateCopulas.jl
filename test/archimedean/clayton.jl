b6a7aeb3791b650cddc925cd82b90a868c742c0c
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2022-01-10 10:57:52 +0100

    Update test samples

--- test/archimedean/clayton.jl
+++ test/archimedean/clayton.jl
@@ -1,3 +1,5 @@
+n = 10^6
+
 @testset "Clayton" begin
     @testset "τ" begin
         @test τ(Clayton(-1 / 2)) == -1 / 3
@@ -8,26 +10,26 @@
 
     @testset "sample" begin
         c = Clayton(-1)
-        u = BivariateCopulas.sample(c, 10000)
+        u = BivariateCopulas.sample(c, n)
         @test corkendall(u') ≈ [1.0 τ(c); τ(c) 1.0] atol = 0.01
 
         c = Clayton(2)
-        u = BivariateCopulas.sample(c, 10000)
+        u = BivariateCopulas.sample(c, n)
         @test corkendall(u') ≈ [1.0 τ(c); τ(c) 1.0] atol = 0.01
 
         c = Clayton(10)
-        u = BivariateCopulas.sample(c, 10000)
+        u = BivariateCopulas.sample(c, n)
         @test corkendall(u') ≈ [1.0 τ(c); τ(c) 1.0] atol = 0.01
     end
 
     @testset "rosenblatt" begin
         c = Clayton(2)
-        u = BivariateCopulas.sample(c, 10000)
+        u = BivariateCopulas.sample(c, n)
 
         @test corkendall(rosenblatt(u, c)') ≈ [1.0 0.0; 0.0 1.0] atol = 0.01
 
         c = Clayton(10)
-        u = BivariateCopulas.sample(c, 10000)
+        u = BivariateCopulas.sample(c, n)
 
         @test corkendall(rosenblatt(u, c)') ≈ [1.0 0.0; 0.0 1.0] atol = 0.01
     end
